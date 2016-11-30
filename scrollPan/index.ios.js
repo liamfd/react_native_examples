2ffab27052e5c71ad0dab6765218277eee962174
Liam Doran <liam@minibardelivery.com>
2016-11-29 19:42:34 -0500

    updated example

--- scrollPan/index.ios.js
+++ scrollPan/index.ios.js
@@ -18,7 +18,7 @@ export default class scrollPan extends Component {
   constructor(props){
     super(props);
     this.wrapperPanResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (e, g) => true,
+      onStartShouldSetPanResponder: (e, g) => false,
       onPanResponderGrant: () => {
         console.log('GRANTED TO WRAPPER');
       },
@@ -40,7 +40,7 @@ export default class scrollPan extends Component {
   render() {
     return (
       <View style={styles.container} {...this.wrapperPanResponder.panHandlers}>
-        <ScrollView onScroll={console.log('scrolled')} style={{maxHeight: 350}} {...this.scollerPanResponder.panHandlers}>
+        <ScrollView onScroll={console.log('scrolled')} style={styles.scroll_view} {...this.scollerPanResponder.panHandlers}>
           <Text style={{fontSize:96}}>Scroll this</Text>
           <Text style={{fontSize:96}}>Scroll this</Text>
           <Text style={{fontSize:96}}>Scroll this</Text>
@@ -59,6 +59,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  scroll_view: {
+    backgroundColor: 'teal',
+    maxHeight: 350
+  }
 });
 
 AppRegistry.registerComponent('scrollPan', () => scrollPan);
